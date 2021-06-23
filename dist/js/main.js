e2334d33da33bfcbda67f12076d2a515247f4de1
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-23 15:44:15 +0530

    Menu Item Update Added.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('.nav')
+const menuNav = document.querySelector('menu-nav');
+
 
 let showMenu = false;
 
@@ -7,9 +10,13 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu(){
     if(!showMenu){
         hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open')
         showMenu = true;
     } else {
         hamburger.classList.remove('open')
+        nav.classList.remove('open');
+        menuNav.classList.remove('open')
         showMenu = false;
     }
 }
